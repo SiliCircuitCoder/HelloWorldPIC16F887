1be97daba1cbbf0a1f59a81c8d0344bbd5204669
SiliCircuitCoder <erick.amaral2851@alumnos.udg.mx>
2024-07-06 16:38:45 -0600

    First_Commit_MikroC

---
+++ HelloWorldPIC16F887.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;HelloWorldPIC16F887.c,1 :: 		void main() {
+;HelloWorldPIC16F887.c,3 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ HelloWorldPIC16F887.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/erick/Documents/HelloWorldPIC16F887/HelloWorldPIC16F887/HelloWorldPIC16F887.c"
+void main() {
+
+}
